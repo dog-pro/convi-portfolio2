cba6de0783c23fb978d88a26035dba8b9142e66a
msms <msr.warden@gmail.com>
2023-11-02 00:32:16 +0900

    commit

--- assets/javaScript/hamburger.js
+++ assets/javaScript/hamburger.js
@@ -1,4 +1,6 @@
 const hamburgerMenu = document.querySelector('.p-nav__hamburger');
+// const hamburgerBody = document.querySelector('.p-nav__hamburger');
 hamburgerMenu.addEventListener('click', () => {
     hamburgerMenu.classList.toggle('active');
+    // hamburgerBody.classList.toggle('active');
 });
